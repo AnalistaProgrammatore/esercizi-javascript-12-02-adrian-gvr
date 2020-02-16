a65203d45f3a5e79f1cc76f84baea87840cf5303
Adrian <59113139+adrian-gvr@users.noreply.github.com>
2020-02-16 21:41:33 +0100

    Add files via upload

--- fattoriale.js
+++ fattoriale.js
@@ -1,3 +1,7 @@
-/**
-* Scrivere qui il codice della funzione fattoriale(n)
-**/
+function fatt(n) {
+    if (n === 0)
+        return 1;
+    else
+        return n * (fatt(n - 1));
+}
+console.log(fatt(5));
